1b23fe0ad74ee532a043f3dc9b5531ac00d2617e
leonardoo <leonardoorozcop@gmail.com>
2015-07-25 15:20:29 -0500

    add posibiliti for url with -

--- apps/manga/urls/manga.py
+++ apps/manga/urls/manga.py
@@ -4,6 +4,6 @@ from .. import views
 
 MANGA = [
     url(r'^listado/$', views.MangaListView.as_view(), name="list-mangas"),
-    url(r'^(?P<name>\w+)/$', views.MangaDetailView.as_view(),
+    url(r'^(?P<name>(\w|-)+)/$', views.MangaDetailView.as_view(),
         name="detail-manga"),
 ]
