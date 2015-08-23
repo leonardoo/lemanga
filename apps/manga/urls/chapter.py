c057146ebcc3f8871f8b06b3680c91ecb1624b8d
leonardoo <leonardoorozcop@gmail.com>
2015-08-23 00:52:21 -0500

    urls for chapter

--- apps/manga/urls/chapter.py
+++ apps/manga/urls/chapter.py
@@ -5,4 +5,8 @@ from .. import views
 CHAPTER = [
     url(r'^d/(?P<name>(\w|-)+)/create/$', views.manga_create_chapter,
         name="create-chapter"),
+    url(r'^d/(?P<name>(\w|-)+)/(?P<chapter>\d+)/(?P<user>\d+)/(?P<number>\d+)/$',
+        views.ChapterDetailView.as_view(),
+        name="manga-chapter"),
+
 ]
