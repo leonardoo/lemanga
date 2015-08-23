945c7c2820ea4e5b8feae9b4ed9e90e77350eb4a
leonardoo <leonardoorozcop@gmail.com>
2015-08-23 00:51:20 -0500

    slug name must be unique

--- apps/manga/models/manga.py
+++ apps/manga/models/manga.py
@@ -14,7 +14,7 @@ class Manga(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200, unique=True)
-    slug = AutoSlugField(populate_from='name')
+    slug = AutoSlugField(populate_from='name', unique=True, always_update=True)
 
     class Meta:
         verbose_name = "Manga"
